430fb098d717841128896830cc6f4115b237eecb
rulhuda <nurulhuda.unhasy@gmail.com>
2022-09-22 12:00:11 +0700

    Deleting Footer

--- src/components/NotesApp.jsx
+++ src/components/NotesApp.jsx
@@ -9,7 +9,7 @@ import Navigation from "./Navigation";
 function NotesApp () {
     return (
       <div className="app-container bg-dark">
-        <header>
+        <header className="sticky-top bg-dark">
           <h1><Link to="/" style={{textDecoration: "none"}}>Aplikasi Catatan</Link></h1>
           <Navigation />
         </header>
@@ -21,13 +21,6 @@ function NotesApp () {
             <Route path="/notes/:id" element={<DetailPage />} />
           </Routes>
         </main>
-        <footer className="bg-light text-dark text-center m-0 p-3">
-          <h3 className="text-center">
-            <Link className="text-dark text-decoration-none" to="https://github.com/rulhuda">Nurul Huda</Link>
-          </h3>
-            <hr />
-          <h2>{new Date().getFullYear()}</h2>
-        </footer>
       </div>
     );
 }
